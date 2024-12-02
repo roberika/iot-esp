cceabbbb1b5ba06aee2b1ded5578485a6e37e93a
Robert Antonius <robert.antonius23@gmail.com>
2024-12-02 18:33:01 +0700

    Tinggal di tes

--- dht-sensor/src/main.cpp
+++ dht-sensor/src/main.cpp
@@ -3,20 +3,24 @@
 #include <WiFiClientSecure.h>
 #include <FirebaseClient.h>
 #include "DHT.h"
+#include <NTPClient.h>
 #include <Adafruit_Sensor.h>
  
 #define WIFI_SSID "Wifi Rumah" 
 #define WIFI_PASSWORD "1992kunci" 
 #define DHT_TYPE DHT11  
-// Cloud Firestore
-#define FIREBASE_PROJECT_ID "PROJECT_ID"
-#define API_KEY "Web_API_KEY"
+#define FIREBASE_PROJECT_ID "dht-firebase-if51"
+#define API_KEY "AIzaSyDwOZzT_L0wlNby5OrAscTdL8sBKDmZ1ik"
 #define THRESHOLD_COLLECTION_ID "threshold"
 #define RECORDS_COLLECTION_ID "records"
 #define RECORDS_INTERVAL 60000
-// Realtime Database
-#define DATABASE_URL "URL"
+#define USER_EMAIL "dht-firebase@if51.mdp.ac.id"
+#define USER_PASSWORD "janganlupo"
+#define DATABASE_URL "https://dht-firebase-if51-default-rtdb.asia-southeast1.firebasedatabase.app/"
+#define DATABASE_SECRET "seXLmFU5gfHCwQ8SE4Y9yK1iDxLv1CAMJZtAkTed"
 #define MONITORING_INTERVAL 1000
+void authHandler();
+String getTimestampString(uint64_t sec, uint32_t nano);
 
 // TODO: Belum di cek pin berapa
 #define BUZZER 4;
@@ -25,14 +29,16 @@
 
 DHT dhtLeft(DHT_LEFT, DHT_TYPE);
 DHT dhtRight(DHT_RIGHT, DHT_TYPE);
+
 DefaultNetwork network; 
 FirebaseApp app;
-// TODO: Can't use no auth :( Probably using UserAuth then
-NoAuth noAuth;
+UserAuth userAuth(API_KEY, USER_EMAIL, USER_PASSWORD);
+LegacyToken legacyToken(DATABASE_SECRET);
 RealtimeDatabase database;
 AsyncResult resultRealtime;
 Firestore::Documents docs;
 WiFiClientSecure sslFirestore, sslRealtime;
+NTPClient timeClient(sslFirestore, "pool.ntp.org");
 AsyncClientClass clientFirestore(sslFirestore, getNetwork(network)), clientRealtime(sslRealtime, getNetwork(network));
 
 double thresholdDHTLeftTemperature;
@@ -44,6 +50,7 @@ double thresholdDHTRightHumidity;
 double recordedDHTRightTemperature;
 double recordedDHTRightHumidity;
 int documentCount;
+unsigned long lastAuthentication = 0;
 unsigned long lastFirestoreUpdate = 0;
 unsigned long lastRealtimeUpdate = 0;
 
@@ -65,17 +72,21 @@ void setup() {
   Serial.println(); 
   Serial.print("Connected to "); 
   Serial.println(WiFi.localIP()); 
+
+  // Konek ke server NTP
+  timeClient.begin();
   
   // Konek ke Firebase
   //.. Bangun header dan inisialisasi
   sslFirestore.setInsecure();
   sslFirestore.setBufferSizes(1024, 1024);
-  initializeApp(clientFirestore, app, getAuth(noAuth));
+  initializeApp(clientFirestore, app, getAuth(userAuth));
   sslRealtime.setInsecure();
   sslRealtime.setBufferSizes(1024, 1024);
-  initializeApp(clientRealtime, app, getAuth(noAuth));
+  initializeApp(clientRealtime, app, getAuth(legacyToken));
 
   //.. Konek ke stream realtime database
+  authHandler();
   app.getApp<RealtimeDatabase>(database);
   database.url(DATABASE_URL);
   clientRealtime.setAsyncResult(resultRealtime);
@@ -93,6 +104,7 @@ void setup() {
 } 
  
 void loop() {
+  authHandler();
   app.loop();
 
   //.. TODO: Ambil jumlah record pada Firebase
@@ -107,21 +119,18 @@ void loop() {
   recordedDHTRightHumidity = dhtRight.readHumidity();
 
   // Ambil nilai threshold yang disimpan pada Firebase
-  //.. TODO: Ambil Document dht1 dan dht2 pada Collection 
+  //.. Ambil Document dht1 dan dht2 pada Collection 
   //.. threshold yang berisi threshold peringatan buzzer
   String payload = docs.list(clientFirestore, Firestore::Parent(FIREBASE_PROJECT_ID), THRESHOLD_COLLECTION_ID);
 
   //.. Tampilkan payload atau error
-  if (clientFirestore.lastError().code() == 0)
-    Serial.println(payload);
-  else
-    printError(clientFirestore.lastError().code(), clientFirestore.lastError().message());
+  if (clientFirestore.lastError().code() == 0) Serial.println(payload);
 
   //.. TODO: Muat threshold
-  thresholdDHTLeftTemperature = 0;
-  thresholdDHTLeftHumidity = 0;
-  thresholdDHTRightTemperature = 0;
-  thresholdDHTRightHumidity = 0;
+  thresholdDHTLeftTemperature = 30;
+  thresholdDHTLeftHumidity = 30;
+  thresholdDHTRightTemperature = 30;
+  thresholdDHTRightHumidity = 30;
 
   // Cek apakah suhu dan kelembaban melebihi nilai threshold
   if((recordedDHTLeftHumidity >= thresholdDHTLeftHumidity && recordedDHTLeftTemperature >= thresholdDHTLeftTemperature) ||
@@ -131,21 +140,21 @@ void loop() {
     digitalWrite(BUZZER, LOW);
   }
 
-  // TODO: Simpan rekaman catatan ke Realtime Database
-  if (millis() - lastRealtimeUpdate > MONITORING_INTERVAL || ms == 0) {
+  // Simpan rekaman catatan ke Realtime Database
+  if (millis() - lastRealtimeUpdate > MONITORING_INTERVAL) {
     lastRealtimeUpdate = millis();
-    database.set<int>(clientRealtime, "/test/int", recordedDHTLeftHumidity);
-    database.set<int>(clientRealtime, "/test/int", recordedDHTLeftTemperature);
-    database.set<int>(clientRealtime, "/test/int", recordedDHTRightHumidity);
-    database.set<int>(clientRealtime, "/test/int", recordedDHTRightTemperature);
+    database.set<double>(clientRealtime, "/leftHumidity", recordedDHTLeftHumidity);
+    database.set<double>(clientRealtime, "/leftTemperature", recordedDHTLeftTemperature);
+    database.set<double>(clientRealtime, "/rightHumidity", recordedDHTRightHumidity);
+    database.set<double>(clientRealtime, "/rightTemperature", recordedDHTRightTemperature);
   }
 
   // Simpan rekamanan catatan ke Firestore Database
-  if (millis() - lastFirestoreUpdate > RECORDS_INTERVAL || ms == 0) {
+  if (millis() - lastFirestoreUpdate > RECORDS_INTERVAL) {
     lastFirestoreUpdate = millis();
     for(int i = 0; i < 2; i++) {
       //.. TODO: Susun jadi 1 dokumen
-      Values::TimestampValue recordTime(getTimestampString(1712674441, 999999999));
+      Values::TimestampValue recordTime(getTimestampString(getTime(), 999999999));
       Values::DoubleValue recordTemperature((i == 0) ? recordedDHTLeftTemperature : recordedDHTRightTemperature);
       Values::DoubleValue recordHumidity((i == 0) ? recordedDHTLeftHumidity : recordedDHTRightHumidity);
       Values::IntegerValue recordDHTID(i);
@@ -167,6 +176,26 @@ void loop() {
   delay(10);
 }
 
+// Untuk autentikasi
+void authHandler()
+{
+  // Blocking authentication handler with timeout
+  unsigned long ms = millis();
+  while (app.isInitialized() && !app.ready() && millis() - lastAuthentication < 120 * 1000)
+  {
+    // The JWT token processor required for ServiceAuth and CustomAuth authentications.
+    // JWT is a static object of JWTClass and it's not thread safe.
+    // In multi-threaded operations (multi-FirebaseApp), you have to define JWTClass for each FirebaseApp,
+    // and set it to the FirebaseApp via FirebaseApp::setJWTProcessor(<JWTClass>), before calling initializeApp.
+    JWT.loop(app.getAuth());
+  }
+}
+
+unsigned long getTime() {
+  timeClient.update();
+  unsigned long now = timeClient.getEpochTime();
+  return now;
+}
 
 // Untuk timestamp
 String getTimestampString(uint64_t sec, uint32_t nano)
