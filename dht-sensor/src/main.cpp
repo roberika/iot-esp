5fb3e4df273d16d6e11b17adc9e9c7c8f9b7f5b1
Robert Antonius <robert.antonius23@gmail.com>
2024-12-06 20:15:02 +0700

    It's done, I just need to resolder parts

--- dht-sensor/src/main.cpp
+++ dht-sensor/src/main.cpp
@@ -2,7 +2,9 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <FirebaseClient.h>
-// #include "DHT.h"
+#include <nlohmann/json.hpp>
+#include <Adafruit_Sensor.h>
+#include "DHT.h"
 #include "time.h"
  
 #define WIFI_SSID "Wifi Rumah" 
@@ -10,30 +12,25 @@
 #define DHT_TYPE DHT11  
 #define FIREBASE_PROJECT_ID "dht-firebase-if51"
 #define API_KEY "AIzaSyDwOZzT_L0wlNby5OrAscTdL8sBKDmZ1ik"
-#define THRESHOLD_COLLECTION_ID "threshold/dht1"
+#define THRESHOLD_COLLECTION_ID "thresholds/threshold"
+#define THRESHOLD_INTERVAL 5000
 #define RECORDS_COLLECTION_ID "records"
 #define RECORDS_INTERVAL 10000
-// #define USER_EMAIL "dht-firebase@if51.mdp.ac.id"
-// #define USER_PASSWORD "janganlupo"
-#define USER_EMAIL "robatononihon@gmail.com"
-#define USER_PASSWORD "woweee"
+#define USER_EMAIL "dht-firebase@if51.mdp.ac.id"
+#define USER_PASSWORD "janganlupo"
+// #define USER_EMAIL "robatononihon@gmail.com"
+// #define USER_PASSWORD "woweee"
 #define DATABASE_URL "https://dht-firebase-if51-default-rtdb.asia-southeast1.firebasedatabase.app/"
 #define DATABASE_SECRET "seXLmFU5gfHCwQ8SE4Y9yK1iDxLv1CAMJZtAkTed"
 #define MONITORING_INTERVAL 1000
-void authHandler(FirebaseApp appFirestore);
-String getTimestampString(uint64_t sec);
-unsigned long getTime();
-void printResult(AsyncResult &aResult);
+#define BUZZER 4
+#define DHT_LEFT 14
+#define DHT_RIGHT 5
 
-// TODO: Belum di cek pin berapa
-#define BUZZER 4;
-#define DHT_LEFT 4;
-#define DHT_RIGHT 4;
-
-// DHT dhtLeft(DHT_LEFT, DHT_TYPE);
-// DHT dhtRight(DHT_RIGHT, DHT_TYPE);
-
-// TODO: Having auth problems, switching to ServiceAuth tommorow
+// Having auth problems, switching to ServiceAuth tommorow
+// Nevermind userAuth works
+DHT dhtLeft(DHT_LEFT, DHT_TYPE);
+DHT dhtRight(DHT_RIGHT, DHT_TYPE);
 DefaultNetwork network; 
 FirebaseApp appFirestore, appRealtime;
 UserAuth userAuth(API_KEY, USER_EMAIL, USER_PASSWORD, 3000);
@@ -42,7 +39,15 @@ RealtimeDatabase database;
 AsyncResult resultFirestore, resultRealtime;
 Firestore::Documents docs;
 WiFiClientSecure sslFirestore, sslRealtime;
-AsyncClientClass clientFirestore(sslFirestore, getNetwork(network)), clientRealtime(sslRealtime, getNetwork(network));
+using AsyncClient = AsyncClientClass;
+AsyncClient clientFirestore(sslFirestore, getNetwork(network)), clientRealtime(sslRealtime, getNetwork(network));
+using json = nlohmann::json;
+
+void authHandler(FirebaseApp appFirestore);
+String getTimestampString(uint64_t sec);
+unsigned long getTime();
+void printPayload(AsyncClient client, String payload);
+void printResult(AsyncResult &aResult);
 
 double thresholdDHTLeftTemperature;
 double thresholdDHTLeftHumidity;
@@ -52,6 +57,7 @@ double thresholdDHTRightTemperature;
 double thresholdDHTRightHumidity;
 double recordedDHTRightTemperature;
 double recordedDHTRightHumidity;
+unsigned long lastThresholdUpdate = 0;
 unsigned long lastFirestoreUpdate = 0;
 unsigned long lastRealtimeUpdate = 0;
 unsigned long epochTime; 
@@ -61,9 +67,9 @@ void setup() {
   // Setup perangkat
   Serial.begin(115200); 
   Serial.println(" ");
-  // pinMode(BUZZER, OUTPUT);
-  // dhtLeft.begin();
-  // dhtRight.begin();
+  pinMode(BUZZER, OUTPUT);
+  dhtLeft.begin();
+  dhtRight.begin();
  
   // Konek ke internet 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD); 
@@ -78,7 +84,7 @@ void setup() {
   Serial.println(WiFi.localIP()); 
 
   // Konek ke server NTP
-  configTime(0, 3600*7, ntpServer);
+  configTime(0, 0, ntpServer);
   epochTime = getTime();
   
   // Konek ke Firebase
@@ -113,59 +119,71 @@ void setup() {
 } 
  
 void loop() {
-  // Serial.println("New loop new me");
-  Serial.println("Auth Firestore");
+  
+  if (!appFirestore.ready() || !appRealtime.ready()) {
+    return;
+  }
+
+  Serial.println("Loop start");
+  // Serial.println("Auth Firestore");
   authHandler(appFirestore);
-  Serial.println("Loop database");
+  // Serial.println("Loop database");
   database.loop();
-  Serial.println("Loop docs");
+  // Serial.println("Loop docs");
   docs.loop();
   // Serial.println("Kenja time");
 
-  // Ambil data suhu dan kelembaban dari dht
-  recordedDHTLeftTemperature = 40;
-  recordedDHTLeftHumidity = 34;
-  recordedDHTRightTemperature = 35;
-  recordedDHTRightHumidity = 36;
-  // recordedDHTLeftTemperature = dhtLeft.readTemperature(false);
-  // recordedDHTLeftHumidity = dhtLeft.readHumidity();
-  // recordedDHTRightTemperature = dhtRight.readTemperature(false);
-  // recordedDHTRightHumidity = dhtRight.readHumidity();
-
   // Ambil nilai threshold yang disimpan pada Firebase
-  //.. Ambil Document dht1 dan dht2 pada Collection 
-  //.. threshold yang berisi threshold peringatan buzzer
-  String payload = docs.get(clientFirestore, Firestore::Parent(FIREBASE_PROJECT_ID), THRESHOLD_COLLECTION_ID, GetDocumentOptions(DocumentMask()));
+  if (millis() - lastThresholdUpdate > THRESHOLD_INTERVAL) {
+    lastThresholdUpdate = millis();
 
-  //.. Tampilkan payload atau error
-  Serial.println("Fetching...");
-  if (clientFirestore.lastError().code() == 0) {
-    Serial.println("Nggak error");
-    Serial.println(payload);
-  } else {
-    Serial.println("Error wkwwkwkwkw");
-    Firebase.printf("Error, msg: %s, code: %d\n", clientFirestore.lastError().message().c_str(), clientFirestore.lastError().code());
+    //.. Ambil Document dht1 dan dht2 pada Collection 
+    //.. threshold yang berisi threshold peringatan buzzer
+    Serial.println("Fetching...");
+    String payload = docs.get(clientFirestore, Firestore::Parent(FIREBASE_PROJECT_ID), THRESHOLD_COLLECTION_ID, GetDocumentOptions(DocumentMask()));
+
+    //.. Tampilkan payload atau error
+    // printPayload(clientFirestore, payload);
+    // printResult(resultFirestore);
+
+    //.. Muat threshold
+    json decoded = json::parse(payload);
+    json threshold = decoded.at("fields");
+    Serial.println("Thresholds...");
+    thresholdDHTLeftTemperature = threshold.at("leftTemperature").at("doubleValue").template get<double>();
+    thresholdDHTLeftHumidity = threshold.at("leftHumidity").at("doubleValue").template get<double>();
+    thresholdDHTRightTemperature = threshold.at("rightTemperature").at("doubleValue").template get<double>();
+    thresholdDHTRightHumidity = threshold.at("rightHumidity").at("doubleValue").template get<double>();
+    Serial.println(thresholdDHTLeftTemperature);
+    Serial.println(thresholdDHTLeftHumidity);
+    Serial.println(thresholdDHTRightTemperature);
+    Serial.println(thresholdDHTRightHumidity);
+    Serial.println(" ");
   }
-  printResult(resultFirestore);
-  Serial.println(" ");
-  // yield();
 
-  //.. TODO: Muat threshold
-  thresholdDHTLeftTemperature = 30;
-  thresholdDHTLeftHumidity = 30;
-  thresholdDHTRightTemperature = 30;
-  thresholdDHTRightHumidity = 30;
+  // Ambil data suhu dan kelembaban dari dht
+  recordedDHTLeftTemperature = dhtLeft.readTemperature(false);
+  recordedDHTLeftHumidity = dhtLeft.readHumidity();
+  recordedDHTRightTemperature = dhtRight.readTemperature(false);
+  recordedDHTRightHumidity = dhtRight.readHumidity();
+  Serial.println("Records...");
+  Serial.println(recordedDHTLeftTemperature);
+  Serial.println(recordedDHTLeftHumidity);
+  Serial.println(recordedDHTRightTemperature);
+  Serial.println(recordedDHTRightHumidity);
+  Serial.println(" ");
 
   // Cek apakah suhu dan kelembaban melebihi nilai threshold
-  // if((recordedDHTLeftHumidity >= thresholdDHTLeftHumidity && recordedDHTLeftTemperature >= thresholdDHTLeftTemperature) ||
-  //   (recordedDHTRightHumidity >= thresholdDHTRightHumidity && recordedDHTRightTemperature >= thresholdDHTRightTemperature)) {
-  //   digitalWrite(BUZZER, HIGH);
-  // } else {
-  //   digitalWrite(BUZZER, LOW);
-  // }
+  if((recordedDHTLeftHumidity >= thresholdDHTLeftHumidity && recordedDHTLeftTemperature >= thresholdDHTLeftTemperature) ||
+    (recordedDHTRightHumidity >= thresholdDHTRightHumidity && recordedDHTRightTemperature >= thresholdDHTRightTemperature)) {
+    digitalWrite(BUZZER, HIGH);
+    Serial.println("DANGER!!!");
+  } else {
+    digitalWrite(BUZZER, LOW);
+    Serial.println("calm");
+  }
 
-  // This works fine
-  // Simpan rekaman catatan ke Realtime Database
+  // Simpan data pengukuran ke Realtime Database
   if (millis() - lastRealtimeUpdate > MONITORING_INTERVAL) {
     lastRealtimeUpdate = millis();
     Serial.println("Updating...");
@@ -180,12 +198,12 @@ void loop() {
     Serial.println(" ");
   }
 
-  // Simpan rekamanan catatan ke Firestore Database
+  // Simpan rekaman catatan ke Firestore Database
   if (millis() - lastFirestoreUpdate > RECORDS_INTERVAL) {
     lastFirestoreUpdate = millis();
     for(int i = 0; i < 2; i++) {
       //.. Susun jadi 1 dokumen
-      Serial.println("Time...");
+      Serial.println("Timestamp...");
       Values::TimestampValue recordTime(getTimestampString(getTime()));
       Serial.println("Temperature...");
       Values::DoubleValue recordTemperature((i == 0) ? recordedDHTLeftTemperature : recordedDHTRightTemperature);
@@ -195,7 +213,7 @@ void loop() {
       Values::IntegerValue recordDHTID(i);
       Serial.println("Combined...");
       Document<Values::Value> doc("dhtid", Values::Value(recordDHTID));
-      doc.add("time", Values::Value(recordTime));
+      doc.add("timestamp", Values::Value(recordTime));
       doc.add("temperature", Values::Value(recordTemperature));
       doc.add("humidity", Values::Value(recordHumidity));
 
@@ -206,34 +224,26 @@ void loop() {
       // documentID tu terpisah dari documentPath, jadi harusny ini buat ID acak dewek
       // karena https://github.com/mobizt/FirebaseClient/blob/main/resources/docs/firestore_database.md#-string-createdocumentasyncclientclass-aclient-firestoreparent-parent-const-string-documentpath-documentmask-mask-documentvaluesvalue-document
       String documentPath = RECORDS_COLLECTION_ID;
-      String payload = docs.createDocument(clientFirestore, Firestore::Parent(FIREBASE_PROJECT_ID), documentPath, DocumentMask(), doc);
-      //.. Tampilkan payload atau error
       Serial.println("Saving...");
-      if (clientFirestore.lastError().code() == 0) {
-        Serial.println("Nggak error");
-        Serial.println(payload);
-      } else {
-        Serial.println("Error wkwwkwkwkw"); 
-        Firebase.printf("Error, msg: %s, code: %d\n", clientFirestore.lastError().message().c_str(), clientFirestore.lastError().code());
-      }
-      documentCount++;
-      // yield();
+      String payload = docs.createDocument(clientFirestore, Firestore::Parent(FIREBASE_PROJECT_ID), documentPath, DocumentMask(), doc);
+      
+      //.. Tampilkan payload atau error
+      // printPayload(clientFirestore, payload);
       Serial.println(" ");
     }
   }
 
+  // Ternyata crash karna delay alamak
   // Delay supaya tidak spam 10 milidetik
-  // Serial.println("I'm going to delay");
-  delay(1000);
-  // Serial.println("I delayed");
+  // delay(1000);
 }
 
 // Untuk autentikasi
-void authHandler(FirebaseApp appFirestore)
+void authHandler(FirebaseApp app)
 {
   // Blocking authentication handler with timeout
   unsigned long ms = millis();
-  while (appFirestore.isInitialized() && !appFirestore.ready() && millis() - ms < 120 * 1000)
+  while (app.isInitialized() && !app.ready() && millis() - ms < 120 * 1000)
   {
     // The JWT token processor required for ServiceAuth and CustomAuth authentications.
     // JWT is a static object of JWTClass and it's not thread safe.
@@ -241,8 +251,19 @@ void authHandler(FirebaseApp appFirestore)
     // and set it to the FirebaseApp via FirebaseApp::setJWTProcessor(<JWTClass>), before calling initializeApp.
     // Bukan multithreaded, so WHY
     // Litteray disini cuma salahny
-    JWT.loop(appFirestore.getAuth());
+    JWT.loop(app.getAuth());
     printResult(resultFirestore);
+  }
+}
+
+void printPayload(AsyncClient client, String payload)
+{
+  if (client.lastError().code() == 0) {
+    Serial.println("Nggak error");
+    Serial.println(payload);
+  } else {
+    Serial.println("Error wkwwkwkwkw"); 
+    Firebase.printf("Error, msg: %s, code: %d\n", client.lastError().message().c_str(), client.lastError().code());
   }
 }
 
@@ -272,11 +293,7 @@ void printResult(AsyncResult &aResult)
 unsigned long getTime() {
   time_t now;
   struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    //Serial.println("Failed to obtain time");
-    return(0);
-  }
-  time(&now);
+  while (!getLocalTime(&timeinfo)) time(&now);
   return now;
 }
 
